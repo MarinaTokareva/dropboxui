8b837f06dbfefd08ef3ca1138f6ec87108afa0b7
Marinatok <marinatokareva1505@gmail.com>
2022-01-17 15:00:23 +0200

    Second commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// var folder = "images/";
+
+// // $.ajax({
+// //     url: folder,
+// //     success: function(data) {
+// //         $(data).find("a").attr("href", function(i, val) {
+// //             if (val.match(/\.(jpe?g|png|gif)$/)) {
+// //                 $("body").append("<img src='" + folder + val + "'>");
+// //             }
+// //         });
+// //     }
+// // });
+
+// function Gallery() {
+
+//     if (val.match('.png')) {
+//         $(".gallery").append("<img src='" + folder + val + "'>");
+//     }
+// };
+// Gallery();
